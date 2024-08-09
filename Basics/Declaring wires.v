77d2e1e6cccb57d3623c53b1d20755b7758526e8
25priya2004 <146159293+25priya2004@users.noreply.github.com>
2024-08-09 03:07:34 -0700

    Create Declaring wires.v

---
+++ Basics/Declaring wires.v
@@ -0,0 +1,13 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    wire x,y;
+    assign x=a&b;
+    assign y=c&d;
+    assign out=x|y;
+    assign out_n=!out;
+endmodule
